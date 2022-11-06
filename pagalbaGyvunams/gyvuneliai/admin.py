1125cc8361838b448f7631e7cf442d59f628905a
arvydasg <azegaspa@gmail.com>
2022-11-06 08:46:15 +0200

    added Animal and Tag models, registered them

--- pagalbaGyvunams/gyvuneliai/admin.py
+++ pagalbaGyvunams/gyvuneliai/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Animal, Tag
+
+admin.site.register(Animal)
+admin.site.register(Tag)
--- pagalbaGyvunams/gyvuneliai/models.py
+++ pagalbaGyvunams/gyvuneliai/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    
+class Animal(models.Model):
+    title = models.CharField(max_length=200)
+    description = RichTextField(null=True, blank=True)
+    # content = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, blank=False)
+
+    # Tell how you want the info to be sorted and named in
+    # django-admin panel. No need to re-migrate
+    class Meta:
+        ordering = ['-created']
+
+    # Tell django how you want the title of the Blog_post item to be
+    # represented in django-admin panel. No need to re-migrate
+    def __str__(self):
+        return self.title
