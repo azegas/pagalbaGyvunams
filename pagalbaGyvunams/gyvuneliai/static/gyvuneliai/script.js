b94b52eba620a41b1484d7f7397f4589230d89fd
arvydasg <azegaspa@gmail.com>
2022-11-17 21:46:02 +0200

    attempted to create "hide if empty div" feature

--- pagalbaGyvunams/gyvuneliai/static/gyvuneliai/script.js
+++ pagalbaGyvunams/gyvuneliai/static/gyvuneliai/script.js
@@ -3,3 +3,30 @@
 // }
 
 // myFunction();
+
+// trying to select all the elements who have empty inner html(no info
+// in django DB and hide them)
+
+// you know what, for now I am leaving this challenge. I will make it
+// so that majority of the fiedls would be mandatory, so the site will
+// look clean and finished and all the info about the animals will be
+// there.
+
+// var fieldDiv = document.getElementsByClassName("zdarova");
+
+// for (var i = 0; i < fieldDiv.length; i++) {
+//   // fieldDiv[i].innerHTML = "oplia";
+//   // console.log("zdrf");
+//   if (fieldDiv[i].innerHTML.length == 0) {
+//     fieldDiv[i].innerHTML = "Some sample content";
+//   } else {
+//     console.log("nepavyko");
+//   }
+// }
+
+// var laukelis = document.("testukas");
+// var laukelio_laukelis = document.getElementsByTagName("p");
+
+// if (laukelio_laukelis.innerHTML.length == 0) {
+//   laukelio_laukelis.innerHTML = "AAA";
+// }
