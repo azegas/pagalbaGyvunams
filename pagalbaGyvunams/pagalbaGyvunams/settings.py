316ab51ce982c92df3e6275a7c12be20658a5e16
arvydasg <azegaspa@gmail.com>
2022-11-23 22:52:06 +0200

    debug true, give me some deets!

--- pagalbaGyvunams/pagalbaGyvunams/settings.py
+++ pagalbaGyvunams/pagalbaGyvunams/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get("DEBUG")) == "1"  # 1 == True(debug mode on)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "https://pagalbagyvunams-production.up.railway.app/"
